345d38895fe55eaaf7a1af830cbb57ed62e6ae1b
Anthony Averill <anthony.averill@deptagency.com>
2021-06-10 14:33:40 +0100

    Updates to tabbed content which being displayed twice.

--- src/components/tabbed-panels/tabbed-panels.js
+++ src/components/tabbed-panels/tabbed-panels.js
@@ -1,4 +1,4 @@
-(function ($) {
+(function () {
   jQuery(function () {
     $('.scroll-control-next').on('click', function (e) {
       slideNav(this, e, -1);
@@ -33,11 +33,14 @@
 
   function slideNav(clicked, e, direction) {
     e.preventDefault();
+    //.trigger('click');
+    //var nextTab = $('.nav-tabs > .active').next('a.nav-item');
     var parentContainer = $(clicked).closest(".uds-tabbed-panels");
     var parentNav = $(clicked).siblings('.nav-tabs');
     var scrollPosition = parentNav.data('scroll-position') * 1;
     var navItems = parentNav.find('.nav-item').toArray();
     var scrollOffset = parentNav.css("left").replace("px", "") * 1;
+    //console.log(scrollPosition);
     var adjustNavItem = 0;
 
     if (direction == 1 && scrollPosition > 0) {
@@ -50,11 +53,34 @@
 
     scrollOffset = 0;
     for (var i = 0; i < scrollPosition; i++) {
+      console.log($(navItems[i]).outerWidth());
       scrollOffset += $(navItems[i]).outerWidth();
     }
     parentNav.css("left", "-" + scrollOffset + "px");
 
     setControlVisibility(clicked);
+
+    /*
+    if(scrollPosition == 0 && direction == 1) {
+      direction = 0;
+    }
+    if(scrollPosition > 0 && direction == 1) {
+      adjustNavItem = -1;
+    }
+    if(scrollPosition == navItems.length-1 && direction == -1) {
+      direction = 0;
+    }
+    if(direction == 1) {
+
+    }
+    //console.log(navItems);
+    parentNav.css("left", scrollOffset+(direction*($(navItems[scrollPosition+adjustNavItem]).outerWidth())));
+    console.log(scrollOffset+(direction*($(navItems[scrollPosition+adjustNavItem]).outerWidth())));
+    parentNav.data('scroll-position', (scrollPosition)+(direction*-1));
+    console.log((scrollPosition)+(direction*-1));
+
+    console.log("--- end ---");
+    */
   }
 
-}(jQuery));
+})();
