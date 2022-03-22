a440aa5c26aecb114cdad6d89d4336fabb8e1f0a
Travis Butterfield <tbutterf@asu.edu>
2022-03-22 16:59:02 -0700

    Final Sass compilation for release

--- src/components/tabbed-panels/tabbed-panels.js
+++ src/components/tabbed-panels/tabbed-panels.js
@@ -1,5 +1,24 @@
-(function ($) {
+import { googleAnalytics } from '../../../src/js/googleAnalytics';
+
+export const initTabs = function () {
+  'use strict';
   jQuery(function () {
+    $(document).on('click', function (e) {
+      setButtonsCompatibility(e);
+    });
+
+    document.querySelectorAll('.uds-tabbed-panels').forEach((item) => {
+      const nav = item.querySelector('.nav-tabs');
+      nav.addEventListener('scroll', (event) => {
+        const scrollPos = event.target.scrollLeft;
+        const prevButton = item.querySelector('.scroll-control-prev');
+        const nextButton = item.querySelector('.scroll-control-next');
+        const atFarRight = nav.offsetWidth + scrollPos + 3 >= nav.scrollWidth;
+        prevButton.style.display = scrollPos === 0 ? 'none' : 'block';
+        nextButton.style.display = atFarRight ? 'none' : 'block';
+      });
+    });
+
     $('.scroll-control-next').on('click', function (e) {
       if (window.innerWidth > 992) {
         slideNav(this, e, -1);
@@ -12,61 +31,25 @@
       }
     });
 
-    $('.uds-tabbed-panels').each(function () {
-      var panel = $(this);
-      var nav = panel.find('.nav-tabs');
-      nav.on('scroll', function (e) {
-        var scrollPos = e.target.scrollLeft;
-        if (scrollPos === 0) {
-          panel.find('.scroll-control-prev').hide();
-        } else {
-          panel.find('.scroll-control-prev').show();
-        }
-        if (nav.get(0).offsetWidth + scrollPos + 3 >= nav.get(0).scrollWidth) {
-          panel.find('.scroll-control-next').hide();
-        } else {
-          panel.find('.scroll-control-next').show();
-        }
-      });
-    });
-
     $('.uds-tabbed-panels .scroll-control-prev').hide();
 
     if ($('#nav-tab')[0].scrollWidth <= $('.uds-tabbed-panels').width()) {
       $('.uds-tabbed-panels .scroll-control-next').hide();
     }
+  });
 
-    if (window.location.href.indexOf('#') !== -1 && document.getElementById('nav-tab')) {
-      // Get the fragment form the URL
-      const fragment = window.location.href.split('#').at(-1)
-      // Get the pane div
-      const pane = document.getElementById(fragment);
-      // Get the menu item.
-      const menuItem = document.getElementById(fragment + '-tab');
-
-      if (pane && menuItem) {
-        const block = menuItem.closest(".block-inline-blocktabbed-content");
-        // Remove the existing active.
-        block.querySelector('#nav-tab').querySelectorAll('.nav-link').forEach((link) => {
-          link.classList.remove("active");
-          link.ariaSelected = "false";// Does not work
-        });
-        block.querySelector('#nav-tabContent').querySelectorAll('.tab-pane').forEach((tab) => {
-          tab.classList.remove("show","active");
-        });
-
-        pane.classList.add("show","active");
-        menuItem.classList.add("active");
-        menuItem.ariaSelected = "true";
-      }
+  const setButtonsCompatibility = (e) => {
+    const targets = ['a', 'button'];
+    if (targets.includes(e.target.localName)) {
+      e.target.focus();
     }
-    });
+  };
 
-  function setControlVisibility(clicked, scrollOffset) {
-    var parentContainer = $(clicked).closest('.uds-tabbed-panels');
-    var parentNav = $(clicked).siblings('.nav-tabs');
-    var scrollPosition = parentNav.data('scroll-position') * 1;
-    var tabPosition = parentNav[0].scrollWidth - scrollOffset;
+  const setControlVisibility = (clicked, scrollOffset) => {
+    const parentContainer = $(clicked).closest('.uds-tabbed-panels');
+    const parentNav = $(clicked).siblings('.nav-tabs');
+    const scrollPosition = parentNav.data('scroll-position') * 1;
+    const tabPosition = parentNav[0].scrollWidth - scrollOffset;
 
     if (scrollPosition == 0) {
       parentContainer.find('.scroll-control-prev').hide();
@@ -78,14 +61,14 @@
     } else {
       parentContainer.find('.scroll-control-next').show();
     }
-  }
+  };
 
-  function slideNav(clicked, e, direction) {
+  const slideNav = (clicked, e, direction) => {
     e.preventDefault();
-    var parentNav = $(clicked).siblings('.nav-tabs');
-    var scrollPosition = parentNav.data('scroll-position') * 1;
-    var navItems = parentNav.find('.nav-item').toArray();
-    var scrollOffset = parentNav.css('left').replace('px', '') * 1;
+    const parentNav = $(clicked).siblings('.nav-tabs');
+    let scrollPosition = parentNav.data('scroll-position') * 1;
+    const navItems = parentNav.find('.nav-item').toArray();
+    let scrollOffset = parentNav.css('left').replace('px', '') * 1;
     var adjustNavItem = 0;
 
     if (direction == 1 && scrollPosition > 0) {
@@ -100,7 +83,12 @@
     for (var i = 0; i < scrollPosition; i++) {
       scrollOffset += $(navItems[i]).outerWidth();
     }
+
     parentNav.scrollLeft(scrollOffset);
+
     setControlVisibility(clicked, scrollOffset);
-  }
-}(jQuery));
+  };
+
+  // Init goolge analytics
+  googleAnalytics();
+};
